41f97ee9c2e338391c917f59fe8b31ea2279fa17
Siddharth Bansal <49023015+sidbank@users.noreply.github.com>
2021-08-02 14:34:33 +0530

    Initial commit of math.py

---
+++ math.py
@@ -0,0 +1,16 @@
+#Add Function
+def addition:
+    pass
+
+#Substract Function    
+def substraction:
+    pass
+
+#multiply function    
+def multiplication:
+    pass
+    
+#deivison function
+def division:
+    pass
+
